35efbb7148ded1d0a1fb24be5d5a9f208521fd48
Sankalok Sen <ssen2001@connect.hku.hk>
2021-09-26 01:18:44 +0800

    decoder

--- codeitsuisse/routes/stig1.py
+++ codeitsuisse/routes/stig1.py
@@ -11,6 +11,18 @@ logger = logging.getLogger(__name__)
 def evaluateStig1():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
+    
+    from = []
+    to = []
     result = []
+    for q in data['questions']:
+        from.append(q['from'])
+        to.append(q['to'])
+    
+    r = dict()
+    r['p'] = max(from)
+    r['q'] = min(to)
+    result.append(r)
+
     logging.info("My result :{}".format(result))
     return json.dumps(result)
