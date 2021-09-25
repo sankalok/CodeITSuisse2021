29e0d699d092fa8b90c6eab4e1e66bfbcb4eb690
Sankalok Sen <ssen2001@connect.hku.hk>
2021-09-25 22:29:43 +0800

    race

---
+++ codeitsuisse/routes/race.py
@@ -0,0 +1,13 @@
+import logging
+import json
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/fixedrace', methods=['POST'])
+def evaluate():
+    data = request.data
+    return data
