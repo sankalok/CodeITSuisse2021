386749f6b6c8019541bdad0b34f951734183d4a5
Sankalok Sen <ssen2001@connect.hku.hk>
2021-09-25 16:26:21 +0800

    added changes

--- codeitsuisse/routes/asteroid.py
+++ codeitsuisse/routes/asteroid.py
@@ -16,24 +16,32 @@ def evaluateAsteroid():
     for t in data['test_cases']:
         r = {}
         r['input'] = t
-        counter = {}
-        for ch in t:
-            if ch not in counter:
-                counter[ch] = 0
-            counter[ch] += 1
-        score = 0
-        for key in counter.keys():
-            if(counter[key] <= 6):
-                score += counter[key]
-            elif(counter[key] > 6 and counter[key] < 10):
-                score += counter[key] * 1.5
-            elif(counter[key] >= 10):
-                score += counter[key] * 2
-        r['score'] = int(score)
-        mid = len(t) // 2
-        for i in range(mid - 2, mid + 3):
-            if(t[i-1] == t[i+1]):
-                r['origin'] = i
+        uniqueS = ""
+        for ch in r['input']:
+            if(len(uniqueS) == 0):
+                uniqueS += ch
+            else:
+                if uniqueS[len(uniqueS) - 1] != ch:
+                    uniqueS += ch
+        if(uniqueS == uniqueS[::-1]):
+            counter = {}
+            for ch in t:
+                if ch not in counter:
+                    counter[ch] = 0
+                counter[ch] += 1
+            score = 0
+            for key in counter.keys():
+                if(counter[key] <= 6):
+                    score += counter[key]
+                elif(counter[key] > 6 and counter[key] < 10):
+                    score += counter[key] * 1.5
+                elif(counter[key] >= 10):
+                    score += counter[key] * 2
+                r['score'] = int(score)
+            mid = len(t) // 2
+            for i in range(mid - 2, mid + 3):
+                if(t[i-1] == t[i+1]):
+                    r['origin'] = i
         result.append(r)
 
     result = json.dumps(result)
