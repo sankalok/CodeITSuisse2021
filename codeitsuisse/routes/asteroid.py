feceac81b74f586ea293f99461ac75fbed00ef5b
Sankalok Sen <ssen2001@connect.hku.hk>
2021-09-25 14:23:36 +0800

    removed errors

--- codeitsuisse/routes/asteroid.py
+++ codeitsuisse/routes/asteroid.py
@@ -36,6 +36,6 @@ def evaluateAsteroid():
                 r['origin'] = i
         result.append(r)
 
-    result = json.dumps(result, indent = 4)
+    result = json.dumps(result)
     logging.info("My result :{}".format(result))
     return result
