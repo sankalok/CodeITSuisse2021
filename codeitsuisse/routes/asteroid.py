f3497ea00a787c5bf95d47779d1f5de1c62a7687
Sankalok Sen <ssen2001@connect.hku.hk>
2021-09-25 20:53:47 +0800

    error

--- codeitsuisse/routes/asteroid.py
+++ codeitsuisse/routes/asteroid.py
@@ -18,56 +18,57 @@ def evaluateAsteroid():
         r['input'] = t
         uniqueS = []
         for ch in r['input']:
-            if(len(uniqueS) == 0):
+            if (len(uniqueS) == 0):
                 uniqueS += [ch]
             else:
                 if uniqueS[len(uniqueS) - 1] != ch:
                     uniqueS += [ch]
-        indexes = [0]*len(uniqueS)
+        indexes = [0] * len(uniqueS)
         i = 0
         ch = ''
         for j in range(0, len(r['input'])):
-            if(j == 0):
+            if (j == 0):
                 indexes[i] = j
-            if(r['input'][j] != uniqueS[i]):
-                indexes[i+1] = j
-            i += 1
+            if (r['input'][j] != uniqueS[i]):
+                indexes[i + 1] = j
+                i += 1
         s = 0
         l = 1
         for i in range(0, len(uniqueS)):
             for j in range(i, len(uniqueS)):
                 check = 1
-                for k in range(0, (i-j)//2 + 1):
-                    if(uniqueS[i+k] != uniqueS[j-k]):
+                for k in range(0, (i - j) // 2 + 1):
+                    if (uniqueS[i + k] != uniqueS[j - k]):
                         check = 0
-                if(check != 0 and (j-i+1) > l):
+                if (check != 0 and (j - i + 1) > l):
                     s = i
-                    l = j-i+1
+                    l = j - i + 1
         start = s
-        end = s+l
+        end = s + l
         score = 0
         j = end
         indexes += [len(r['input'])]
-        for i in range(start, (end+start)//2):
-            rng = indexes[i+1] - indexes[i] + indexes[j] - indexes[j-1]
-            if(rng <= 6):
+        for i in range(start, (end + start) // 2):
+            rng = indexes[i + 1] - indexes[i] + indexes[j] - indexes[j - 1]
+            if (rng <= 6):
                 score += rng
-            elif(rng >= 7 and rng <= 9):
+            elif (rng >= 7 and rng <= 9):
                 score += (1.5 * rng)
-            elif(rng >= 10):
+            elif (rng >= 10):
                 score += (2 * rng)
             j -= 1
-        rng = (indexes[(start+end-1)//2 + 1] - indexes[(start+end-1)//2])
-        if(rng <= 6):
+        rng = (indexes[(start + end - 1) // 2 + 1] - indexes[(start + end - 1) // 2])
+        if (rng <= 6):
             score += rng
-        elif(rng >= 7 and rng <= 9):
+        elif (rng >= 7 and rng <= 9):
             score += (1.5 * rng)
-        elif(rng >= 10):
+        elif (rng >= 10):
             score += (2 * rng)
-        mid = (indexes[(start+end-1)//2] + indexes[(start+end-1)//2 + 1]) // 2
+        mid = (indexes[(start + end - 1) // 2] + indexes[(start + end - 1) // 2 + 1]) // 2
         r['score'] = int(score)
         r['mid'] = mid
         result.append(r)
-        
+
+
     logging.info("My result :{}".format(result))
     return json.dumps(data)
