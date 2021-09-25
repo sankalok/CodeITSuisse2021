a4c4c30f22e16875f258d6fb5c6546a76508cd30
Sankalok Sen <ssen2001@connect.hku.hk>
2021-09-26 01:38:21 +0800

    decoder

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -18,7 +18,7 @@ def evaluateDecoder():
     slots = data['num_slots']
     sample = random.sample(values, slots)
     #result['answer'] = sample
-    result['answer'] = ['d', 's', 'a', 's', 'h']
+    result['answer'] = ['d', 's', 'h', 'q', 'b']
 
     logging.info("My result :{}".format(result))
     return json.dumps(result)
