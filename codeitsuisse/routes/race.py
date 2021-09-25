516de843d5c20908b7560bc4f17ad8448c2eb0e8
Sankalok Sen <ssen2001@connect.hku.hk>
2021-09-25 23:11:58 +0800

    race

--- codeitsuisse/routes/race.py
+++ codeitsuisse/routes/race.py
@@ -10,5 +10,4 @@ logger = logging.getLogger(__name__)
 @app.route('/fixedrace', methods=['POST'])
 def evaluateRace():
     data = request.get_data()
-    dataArr = data.split(',')
-    return jsonify(dataArr)
+    return data
