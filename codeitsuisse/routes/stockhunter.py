227e20965ebf55147eed5ad33264debc1d20e747
Sankalok Sen <ssen2001@connect.hku.hk>
2021-09-26 05:42:05 +0800

    stockhunter

--- codeitsuisse/routes/stockhunter.py
+++ codeitsuisse/routes/stockhunter.py
@@ -9,12 +9,29 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 def minCost(cost, m, n):
-    if (n < 0 or m < 0):
-        return sys.maxsize
-    elif (m == 0 and n == 0):
-        return cost[m][n]
-    else:
-        return cost[m][n] + min(minCost(cost, m-1, n), minCost(cost, m, n-1) )
+ 
+    # Instead of following line, we can use int tc[m+1][n+1] or
+    # dynamically allocate memoery to save space. The following
+    # line is used to keep te program simple and make it working
+    # on all compilers.
+    tc = [[0 for x in range(C)] for x in range(R)]
+ 
+    tc[0][0] = cost[0][0]
+ 
+    # Initialize first column of total cost(tc) array
+    for i in range(1, m+1):
+        tc[i][0] = tc[i-1][0] + cost[i][0]
+ 
+    # Initialize first row of tc array
+    for j in range(1, n+1):
+        tc[0][j] = tc[0][j-1] + cost[0][j]
+ 
+    # Construct rest of the tc array
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            tc[i][j] = min(tc[i-1][j-1], tc[i-1][j], tc[i][j-1]) + cost[i][j]
+ 
+    return tc[m][n]
 
 @app.route('/stock-hunter', methods=['POST'])
 def evaluateStockHunter():
