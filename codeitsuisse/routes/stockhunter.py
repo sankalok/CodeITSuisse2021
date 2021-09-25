3fe3f2a76322d673501a74b34c904757ccfa039e
Sankalok Sen <ssen2001@connect.hku.hk>
2021-09-26 04:16:42 +0800

    stockhunter

--- codeitsuisse/routes/stockhunter.py
+++ codeitsuisse/routes/stockhunter.py
@@ -9,12 +9,12 @@ logger = logging.getLogger(__name__)
 
 @app.route('/stock-hunter', methods=['POST'])
 def evaluateStockHunter():
-    data = request.get_json()
+    dataList = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input")
-    result = inputValue * inputValue
+    
+    
     logging.info("My result :{}".format(result))
-    return json.dumps(result)
+    return json.dumps(dataList)
 
 
 
