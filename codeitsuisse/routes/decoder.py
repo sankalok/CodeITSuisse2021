4da8ad6eaa51b34e5e74ff235189496aaba40f0c
Sankalok Sen <ssen2001@connect.hku.hk>
2021-09-26 00:32:06 +0800

    decoder

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -12,10 +12,12 @@ logger = logging.getLogger(__name__)
 def evaluateDecoder():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
+
     values = data['possible_values']
     slots = data['num_slots']
     sample = random.sample(values, slots)
     result['answer'] = sample
+
     logging.info("My result :{}".format(result))
-    return json.dumps(result)
+    return json.dumps(data)
 
