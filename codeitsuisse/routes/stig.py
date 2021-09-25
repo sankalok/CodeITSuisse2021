354faf37c562ff23c5cb3a620d16502cf3a51c1a
Sankalok Sen <ssen2001@connect.hku.hk>
2021-09-26 01:25:34 +0800

    decoder

--- codeitsuisse/routes/stig.py
+++ codeitsuisse/routes/stig.py
@@ -8,20 +8,20 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 @app.route('/stig/perry', methods=['POST'])
-def evaluateStig1():
+def evaluateStig():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     
-    from = []
-    to = []
+    fr = []
+    t = []
     result = []
     for q in data['questions']:
-        from.append(q['from'])
-        to.append(q['to'])
+        fr.append(q['from'])
+        t.append(q['to'])
     
     r = dict()
-    r['p'] = max(from)
-    r['q'] = min(to)
+    r['p'] = max(fr)
+    r['q'] = min(t)
     result.append(r)
 
     logging.info("My result :{}".format(result))
