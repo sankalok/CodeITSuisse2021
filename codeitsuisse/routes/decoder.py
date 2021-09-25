28d66d176235c7f5ba1fd9b914ec92faae14fa17
Sankalok Sen <ssen2001@connect.hku.hk>
2021-09-26 00:27:39 +0800

    decoder

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -1,6 +1,7 @@
 import logging
 import json
 
+import random
 from flask import request, jsonify
 
 from codeitsuisse import app
@@ -11,7 +12,10 @@ logger = logging.getLogger(__name__)
 def evaluateDecoder():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    result = []
+    values = data['possible_values']
+    slots = data['num_slots']
+    sample = random.sample(values, slots)
+    result['answer'] = sample
     logging.info("My result :{}".format(result))
     return json.dumps(result)
 
