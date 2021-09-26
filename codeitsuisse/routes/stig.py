e0c862b74b9c15e7817a28c40c996996a7921e1a
Sankalok Sen <ssen2001@connect.hku.hk>
2021-09-26 08:54:39 +0800

    stockhunter

--- codeitsuisse/routes/stig.py
+++ codeitsuisse/routes/stig.py
@@ -16,9 +16,8 @@ def evaluateStig():
     
     for data in dataList:
         r = {'p' : 1, 'q' : 50000000}
-        for d in data:
-            r['p'] = max(r['p'], d['from'])
-            r['q'] = min(r['q'], d['to'])
+        r['p'] = max(r['p'], data[0]['from'])
+        r['q'] = min(r['q'], d[0]['to'])
         result.append(r)
 
 
